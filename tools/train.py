109a593770ffd92bc1be7771223bb4f07d420d27
yejoonLee <beeriver1@gmail.com>
2021-07-27 08:29:56 +0000

    Simplify EarlyStopping class

--- tools/train.py
+++ tools/train.py
@@ -16,27 +16,21 @@ class EarlyStopping:
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
-        self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.min_loss = np.Inf
         self.delta = delta
 
     def __call__(self, val_loss, model, path):
-        score = -val_loss
-        if self.best_score is None: # first __call__
-            self.best_score = score
-            self.save_checkpoint(val_loss, model, path)
-        elif score < self.best_score + self.delta: # when model didn't improve
+        if val_loss > self.min_loss - self.delta: # when model didn't improve
             self.counter += 1
             if self.verbose:
                 print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else: # when model improved
-            self.best_score = score
-            self.save_checkpoint(val_loss, model, path)
+            self.min_loss = val_loss
+            self._save_checkpoint(model, path)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model, path):
+    def _save_checkpoint(self, model, path):
         torch.save(model.state_dict(), path)
-        self.val_loss_min = val_loss
