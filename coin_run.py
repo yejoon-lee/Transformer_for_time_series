7d70ee750f0d2757fa519ca63e15b18ff0385cc4
yejoonLee <beeriver1@gmail.com>
2021-07-27 06:42:36 +0000

    Update notebooks for synthetic

--- coin_run.py
+++ coin_run.py
@@ -7,18 +7,18 @@ config = dict(
     stride=10,
     src_len=SRC_LEN,
     tgt_len=TGT_LEN,
-    embedding_dim=64,
+    embedding_dim=32,
     nhead=16,
     num_layers=(2, 2),
-    ts_embed='wavenet',
+    ts_embed='conv',
     pos_embed='fixed',
     batch_size=128,
     lr=5e-4,
     betas=(0.9, 0.98),
-    sch_stepsize=5,
-    sch_gamma=0.6,
-    es_patience=3,
-    n_epoch=20,
+    sch_stepsize=10,
+    sch_gamma=0.75,
+    es_patience=5,
+    n_epoch=50,
 )
 
-synth = RunCoin('transformer_for_time_series_coin_data', config, verbose=2, watch=False, checkpoint_path='checkpoints/coin')
+coin = RunCoin('transformer_for_time_series_coin_data', config, verbose=2, watch=False, checkpoint_path='checkpoints/coin')
